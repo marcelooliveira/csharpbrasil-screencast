4b5cd7ea99e2863da4839c4377546415f3ddc6d3
alexfelipe <alexfelipevieira@gmail.com>
2017-06-23 14:57:54 -0300

    620-csharpbrasil-video2.1-Trabalhando com Datas e Horas

---
+++ CSharpBrasil/CSharpBrasil/TrabalhandoComDatas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2017, 9, 23);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("d"));
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Debug.WriteLine(data.ToString("dd/MM"));
+            Debug.WriteLine(data.ToString("dd/MM/yy"));
+
+            data = new DateTime(2017, 9, 23, 13, 14, 15, 987);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("HH:mm"));
+            Debug.WriteLine(data.ToString("HH:mm:ss.fff"));
+
+        }
+    }
+}
