b330427c57012b2f3963b24686c1ad2ef38494e8
alexfelipe <alexfelipevieira@gmail.com>
2017-06-26 15:24:49 -0300

    620-csharpbrasil-video4.1-Consultando o Webservice do ViaCEP

---
+++ CSharpBrasil/CSharpBrasil/CEP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cep = "01001000";
+            string url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+            string result = new HttpClient().GetStringAsync(url).Result;
+
+            Debug.WriteLine(result);
+        }
+    }
+}
