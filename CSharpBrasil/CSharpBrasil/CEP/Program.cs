39674f7c3a9ef07a27cf2465eb94ff248084ee1b
alexfelipe <alexfelipevieira@gmail.com>
2017-06-26 15:37:47 -0300

    620-csharpbrasil-video4.2-Consultando ViaCEP Com Caelum Stella CSharp

--- CSharpBrasil/CSharpBrasil/CEP/Program.cs
+++ CSharpBrasil/CSharpBrasil/CEP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caelum.Stella.CSharp.Http;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,12 +13,31 @@ namespace CEP
     {
         static void Main(string[] args)
         {
-            string cep = "01001000";
+            string cep = "010010007";
+            //string result = GetEndereco(cep);
+
+            //Debug.WriteLine(result);
+
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Debug.WriteLine(enderecoJson);
+
+            string enderecoXml = viaCEP.GetEnderecoXml(cep);
+            Debug.WriteLine(enderecoXml);
+
+            var task = viaCEP.GetEnderecoJsonAsync(cep);
+            Debug.WriteLine(task.Result);
+
+            var endereco = viaCEP.GetEndereco(cep);
+            Debug.WriteLine(string.Format("Logradouro: {0}, Bairro: {1}", endereco.Logradouro, endereco.Bairro));
+        }
+
+        private static string GetEndereco(string cep)
+        {
             string url = "https://viacep.com.br/ws/" + cep + "/json/";
 
             string result = new HttpClient().GetStringAsync(url).Result;
-
-            Debug.WriteLine(result);
+            return result;
         }
     }
 }
